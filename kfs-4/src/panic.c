5ae9f21dc0fc2c3b1c39277e3c3f4600c0567c51
youngwon son <zerone015@naver.com>
2025-03-29 08:05:42 +0900

    feat: update kernel panic hex dump print format

--- kfs-4/src/panic.c
+++ kfs-4/src/panic.c
@@ -11,7 +11,7 @@ static inline void __hex_dump(struct panic_info *info)
     info->gpr.esp += offsetof(struct panic_info, eflags) - offsetof(struct general_purpose_registers, eax);
     printk("EIP: %x\n", info->eip);
     printk("EFLAGS: %x\n\n", info->eflags);
-    printk("EIP is at %p\n", info->eip);       // 나중에 심볼테이블에서 찾아서 함수의 시작주소 찾아서 함수명으로 바꿔야함
+    // printk("EIP is at %p\n", info->eip); 나중에 심볼테이블에서 찾아서 함수의 시작주소 찾아서 함수명으로 바꿔야함 << 하려 했는데 답 없다 잘못된 주소로 점프한 경우 어떤 함수였는지 ebp없이 절대 못찾음 
     printk("eax: %x ebx: %x ecx: %x edx: %x\n", info->gpr.eax, info->gpr.ebx, info->gpr.ecx, info->gpr.edx);
     printk("esi: %x edi: %x ebp: %x esp: %x\n", info->gpr.esi, info->gpr.edi, info->gpr.ebp, info->gpr.esp);
     printk("Stack: ");
